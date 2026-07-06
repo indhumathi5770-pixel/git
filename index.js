d3c92df832c7b21290e3b22288cda69c44391e72
demo <indhu5770@gmail.com>
2026-07-06 14:04:50 +0530

    Resolved stash conflict

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+console.log("welcome to the git course");
+for(var i=0;i<5;i++){
+    console.log("code");
+}
 console.log("welcome to the git Playlist");
 for(Vari=0;i<5;i++) {
     console.log("code io");
